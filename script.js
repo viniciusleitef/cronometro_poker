47dbc52f897b4b8340b53ab06a37bb34d709d67f
viniciusleitef <viniciusleitealves@gmail.com>
2023-02-15 14:44:34 -0300

    Finalização do cronometro

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let segundo = 0;
-let minuto = 1;
-let hora = 1;
+let minuto = 0;
+let hora = 0;
 
 let pausou;
 
 let clicks = false
 
+if(localStorage.segundo){
+	segundo = localStorage.segundo;
+	minuto = localStorage.minuto;
+	hora = localStorage.hora;	
+	escreverHora();
+}
+
 function start(){
-	pausou = setInterval(counter,1000);
+	pausou = setInterval(counter,10);
 }
 
 function pause(){
@@ -20,6 +27,7 @@ function stop(){
 	minuto = 0;
 	hora = 0;
 	zerar();
+	localStorage.clear();
 }
 
 function zerar(){
@@ -28,28 +36,44 @@ function zerar(){
 	document.getElementById("horas").innerText ="00";
 }
 
+function escreverHora(){
+	document.getElementById("segundos").innerText = segundo;
+	if(minuto<10){
+		document.getElementById("minutos").innerText = "0" + minuto;
+	}else{
+		document.getElementById("minutos").innerText = minuto;
+	}
+	document.getElementById("horas").innerText = "0" + hora;
+}
+
 
 function counter(){
-	if(segundo < 10){
-		document.getElementById("segundos").innerText="0"+ segundo;
+	
+	if(segundo<10){
+		document.getElementById("segundos").innerText = "0" + segundo
+		segundo++;
+	}else if(segundo > 9 & segundo < 60){
+		document.getElementById("segundos").innerText = segundo
+		segundo++;
 	}else{
-		document.getElementById("segundos").innerText=segundo;
-	}
-
-	if(segundo > 59){
 		segundo = 0;
-		if(minuto < 10){
-			document.getElementById("minutos").innerText= "0" + minuto;
-		}else{
-			document.getElementById("minutos").innerText= minuto;
-		}
-		if(minuto > 59){
-			minuto = 0;
-
-			document.getElementById("horas").innerText = "0" + hora;
-			hora++;
-		}
 		minuto++;
+		if(minuto < 10){
+			document.getElementById("minutos").innerText = "0" + minuto
+		}else if(minuto > 9 & minuto < 60){
+			document.getElementById("minutos").innerText = minuto
+		}else{
+			segundo = 0;
+			minuto = 0;
+			document.getElementById("minutos").innerText = "0" + minuto
+			document.getElementById("segundos").innerText = "0" + segundo
+
+			hora++;
+			document.getElementById("horas").innerText = "0" + hora
+		}
 	}
-	segundo++;
+
+	localStorage.setItem('segundo', segundo);
+	localStorage.setItem('minuto', minuto);
+	localStorage.setItem('hora', hora);
 }
